d840b223df40ab79b20c12b8af9b3447d4f7091e
animalelder <wllearned@icloud.com>
2024-03-23 13:43:48 -0700

    render API data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,6 +6,24 @@ export const MainView = () => {
   const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://cine-data-db-04361cdbefbe.herokuapp.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.title,
+            description: movie.description,
+            genre: movie.genre.name,
+            director: movie.director.name,
+            imagePath: movie.imagePath,
+            featured: movie.featured,
+          };
+        });
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,45 @@
+import PropTypes from 'prop-types';
+
 export const MovieView = ({ movie, onCloseClick }) => {
   return (
     <div>
-      <div>
-        <img src={movie.image} alt={movie.title} />
-      </div>
       <div>
         <span>Title: </span>
         <span>{movie.title}</span>
       </div>
       <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>ImagePath: </span>
+        <span>{movie.imagePath}</span>
       </div>
       <div>
         <span>Genre: </span>
         <span>{movie.genre}</span>
       </div>
       <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.director.name}</span>
+      </div>
+      <div>
+        <span>Featured: </span>
+        <span>{movie.featured}</span>
       </div>
       <button onClick={onCloseClick}>Close</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    director: PropTypes.string,
+    imagePath: PropTypes.string,
+    featured: PropTypes.bool,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
